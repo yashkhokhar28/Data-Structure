8b9aaec782e372b66c4b6a81aa2a2f6abc2d40c0
Yash Khokhar <yashkhokhar28@gmail.com>
2022-06-26 09:48:09 +0530

    create n number of matrix for any row and column multiplication

--- Lab 2/multiplicationOfTwoMatrix.java
+++ Lab 2/multiplicationOfTwoMatrix.java
@@ -3,42 +3,60 @@ import java.util.*;
 public class multiplicationOfTwoMatrix{
 	public static void main(String[] args) {
 
-		int row,column,i,j,k;
+		int row1,row2,column2,column1,i,j,k;
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Number Of Rows For First Matrix : ");
-		row = sc.nextInt();
+		row1 = sc.nextInt();
 		System.out.println("Enter Number Of Coulmns For First Matrix : ");
-		column = sc.nextInt();
+		column1 = sc.nextInt();
 
-		int[][] matrix_1 = new int[row][column];
-		int[][] matrix_2 = new int[row][column];
-		int[][] multiplication = new int[row][column];
+		int[][] matrix_1 = new int[row1][column1];
 
 		System.out.println("Enter Elements Of First Matrix : ");
-		for (i=0;i<row;i++ ) {
-			for (j=0;j<column;j++ ) {
+		for (i=0;i<row1;i++ ) {
+			for (j=0;j<column1;j++ ) {
 				matrix_1[i][j] = sc.nextInt();
 			}
-			System.out.println();
 		}
 
 		System.out.println("Enter Number Of Rows For Second Matrix: ");
-		row = sc.nextInt();
+		row2 = sc.nextInt();
 		System.out.println("Enter Number Of Coulmns For Second Matrix : ");
-		column = sc.nextInt();
+		column2 = sc.nextInt();
+
+		int[][] matrix_2 = new int[row2][column2];
 
 		System.out.println("Enter Elements Of Second Matrix : ");
-		for (i=0;i<row;i++ ) {
-			for (j=0;j<column;j++ ) {
+		for (i=0;i<row2;i++ ) {
+			for (j=0;j<column2;j++ ) {
 				matrix_2[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println("--------Matrix-1 Data--------");
+		for(i=0;i<row1;i++){
+			for(j=0;j<column1;j++){
+				System.out.print(matrix_1[i][j]+" ");
 			}
 			System.out.println();
 		}
 
-		for (i=0;i<row;i++ ) {
-			for (j=0;j<column;j++ ) {
-				for (k=0;k<column;k++ ) {
+		System.out.println("--------Matrix-2 Data--------");
+		for(i=0;i<row2;i++){
+			for(j=0;j<column2;j++){
+				System.out.print(matrix_2[i][j]+" ");
+			}
+			System.out.println();	
+		}
+
+		int[][] multiplication = new int[row1][column2];
+
+		System.out.println("--------------Matrix Multiplication---------");
+
+		for (i=0;i<row1;i++ ) {
+			for (j=0;j<column2;j++ ) {
+				for (k=0;k<column1;k++ ) {
 					multiplication[i][j] = multiplication[i][j] + matrix_1[i][k] * matrix_2[k][j];
 				}
 				System.out.println(multiplication[i][j]+"\t");
