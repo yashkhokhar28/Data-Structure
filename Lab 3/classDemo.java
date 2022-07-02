8cfc1fd9262695b8c40cb9b955f2be9674568db5
Yash Khokhar <yashkhokhar28@gmail.com>
2022-07-02 17:23:02 +0530

    created objects

--- Lab 3/classDemo.java
+++ Lab 3/classDemo.java
@@ -1,40 +1,36 @@
 /*Create array of object of class Student_Detail with attributes Enrollment_no, Name, Sem, CPI for 5
 students, scan their information and print it.
 */
-import java.util.*;
-
-
+import java.util.Scanner;
 class Student_Detail{
-	int n = 5;
-	int[] Enrollment_no = new int[n];
-	String[] Name = new String[n];
-	int[] Sem = new int[n];
-	double[] CPI = new double[n];
-
-	Scanner sc = new Scanner(System.in);
-
-	void getDetails(){
-		for (int i=0;i<n;i++ ) {
-		System.out.println("Enter "+i+" Enrollment Number : ");
-		Enrollment_no[i] = sc.nextInt();
-		System.out.println("Enter "+i+" Name : ");
-		Name[i] = sc.next();
-		System.out.println("Enter "+i+" Sem : ");
-		Sem[i] = sc.nextInt();
-		System.out.println("Enter "+i+" CPT : ");
-		CPI[i] = sc.nextDouble();
-		}
-		for (int i=0;i<n;i++ ) {
-			System.out.println("Enrollment Is : "+Enrollment_no[i]);
-			System.out.println("Name Is : "+Name[i]);
-			System.out.println("Sem Is : "+Sem[i]);
-			System.out.println("CPI Is : "+CPI[i]);
-		}
+	long enrollment_no;
+	String name;
+	int sem;
+	double cpi;
+	public Student_Detail(long enrollment_no,String name,int sem,double cpi){
+		this.enrollment_no=enrollment_no;
+		this.name=name;
+		this.sem=sem;
+		this.cpi=cpi;
+	}
+	public void printDetails(){
+		System.out.println("Enrollment No. : "+enrollment_no);
+		System.out.println("Name : "+name);
+		System.out.println("Semester : "+sem);
+		System.out.println("CPI : "+cpi);
 	}
 }
 public class classDemo{
 	public static void main(String[] args) {
-		Student_Detail s1 = new Student_Detail();
-		s1.getDetails();
+		Student_Detail obj[]=new Student_Detail[5];
+		obj[0]=new Student_Detail(1106,"yash",3,10);
+		obj[1]=new Student_Detail(1105,"krish",4,9);
+		obj[2]=new Student_Detail(1104,"deep",5,8);
+		obj[3]=new Student_Detail(1103,"jadu",6,7.90);
+		obj[4]=new Student_Detail(1102,"mangu",7,9.9);
+		for(int i=0;i<5;i++){
+			System.out.println("-----Details of student "+(i+1)+"-----");
+			obj[i].printDetails();
+		}
 	}
 }
