31bf1cf729636cce12b8a069fc2084bb1b7955d7
Yash Khokhar <yashkhokhar28@gmail.com>
2022-07-10 20:02:32 +0530

    Update DemoFunction.java

--- Lab 4/DemoFunction.java
+++ Lab 4/DemoFunction.java
@@ -19,6 +19,18 @@ class DemoFun{
         }
         return arr1[top--];
     }
+    public int peep(int i){
+        if (top-i+1<=0) {
+            System.out.println("Stack Underflow");
+        }
+            return arr1[top-i+1];
+    }
+    public int change(int i,int n){
+        if (top-i+1<=0) {
+            System.out.println("Stack Underflow");
+        }
+            return arr1[top-i+1] = n;
+    }
     public void display(){
         if (top<=-1) {
             System.out.println("Stack Underflow");
@@ -37,20 +49,33 @@ public class DemoFunction{
         DemoFun d1 = new DemoFun();
         Scanner sc=new Scanner(System.in);
         while(true){
-             System.out.println("\n1.for push\n2.for pop\n3. for Display \n4. for Exit");
+             System.out.println("\n1.for push\n2.for pop\n3.for peep\n4.for change\n5. for Display \n6. for Exit");
              int n=sc.nextInt();
              if(n==1){
                 System.out.print("Enter a value for push: ");
-                d1.push(sc.nextInt());
+                n=sc.nextInt();
+                d1.push(n);
              }
              else if (n==2){
                 System.out.println("Element popped is: "+d1.pop());
              }
              else if(n==3){
-                d1.display();
+                System.out.print("Enter a value of i for peep: ");
+                int i = sc.nextInt();
+                d1.peep(i);
              }
-             else if(n==4){
-                break;
+             else if (n==4) {
+                System.out.print("Enter a value of element you want to change :  ");
+                int i = sc.nextInt();
+                System.out.print("Enter a value of element you want to replace :  ");
+                n=sc.nextInt();
+                d1.change(i,n);
+             }
+             else if (n==5) {
+                 d1.display();
+             }
+             else if (n==6) {
+                 break;
              }
         }  
     }
