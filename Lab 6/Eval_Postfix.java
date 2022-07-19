429b33eed4b345516941f06454326306bacb0c04
Yash Khokhar <yashkhokhar28@gmail.com>
2022-07-19 21:27:06 +0530

    Add files via upload

---
+++ Lab 6/Eval_Postfix.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+class Eval_Postfix {
+    static Eval_Postfix e1 = new Eval_Postfix();
+    int[] arr1 = new int[100];
+    static int top = -1;
+
+    public void push(int n) {
+        if (top >= 99) {
+            System.out.println("Stack Overflow");
+        } else {
+            top++;
+            n = arr1[top];
+        }
+    }
+
+    public int pop() {
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+        }
+        return arr1[top--];
+    }
+
+    public static int op(String Postfix) {
+
+        for (int i = 0; i < Postfix.length(); i++) {
+            char n = Postfix.charAt(i);
+            if (Character.isLetter(n)) {
+                e1.push(n);
+            } else {
+                int value1 = e1.pop();
+                int value2 = e1.pop();
+
+                switch (n) {
+                    case '+':
+                        e1.push(value1 + value2);
+                        break;
+                    case '-':
+                        e1.push(value1 - value2);
+                        break;
+                    case '*':
+                        e1.push(value1 * value2);
+                        break;
+                    case '/':
+                        e1.push(value1 / value2);
+                        break;
+                }
+            }
+        }
+
+        return e1.pop();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter PostFix Expression : ");
+        String Postfix = sc.nextLine();
+        System.out.println("Evalution : " + op(Postfix));
+    }
+}
